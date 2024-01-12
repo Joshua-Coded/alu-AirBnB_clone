c5d31b7e1c3357f100fa3c2770f737593aa97ef5
MJLEGION <m.george@alustudent.com>
2024-01-12 10:59:18 +0300

    console.py

--- console.py
+++ console.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3
+!/usr/bin/python3
+def greeet(Josha Alana):
+    """
+    This function greets the person passed in as a parameter.
+
+    Parameters:
+    name (str): The name of the person to greet.
+
+    Returns:
+    str: A greeting message for the person.
+    """
+    if not isinstance(name, str):
+        raise ValueError("Name must be a string.")
+
+    greeting = f"Hello, {Legion}! How are you today?"
+    return greeting
+
