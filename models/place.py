789ef1ca0c84d74995235294eb2e96ba3ddd4237
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-17 05:42:33 +0100

    places erros

--- models/place.py
+++ models/place.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-
-"""This files defines the place model
-it inherits from the basemodel
-"""
+"""This files defines the place model it inherits from the basemodel"""
 
 from models.base_model import BaseModel
-from typing import List
 
 
 class Place(BaseModel):
     """the place model"""
 
     #Attributes
-    city_id: str = ""
-    user_id: str = ""
-    name: str = ""
-    description: str = ""
-    number_rooms: int = 0
-    number_bathrooms: int = 0
-    max_guest: int = 0
-    price_by_night: int = 0
-    latitude: float = 0.0
-    longitude: float = 0.0
-    amenity_ids: list[str] = []
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
