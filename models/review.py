198c3deebfe6f63d96f43b11864347573f520073
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-17 04:32:46 +0100

    the review model error fixed

--- models/review.py
+++ models/review.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-"""This file defines the Review Model
-It inherits from the BaseModel
+"""This file defines the review model
+it inherits from the basemodel
 """
 
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """The Review Model"""
+    """the review model"""
 
-    # Attributes
+    #Attributes
     place_id: str = ""
     user_id: str = ""
     text: str = ""
